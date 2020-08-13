375d50556056e795646bf8b22f10cb97cce1a0e0
pruthvi247 <pruthvikumar247@gmail.com>
2020-08-13 20:09:04 +0530

    git added singly linked list python examples

---
+++ workspace/data_structures_freeCodeCamp/ds_examples/python_examples/singly_linked_list/single_linked_list.py
@@ -0,0 +1,55 @@
+
+## [source: https://stackabuse.com/linked-lists-in-detail-with-python-examples-single-linked-lists/]
+
+from Node import Node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_at_start(self, data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        print(f"new_node.item: {new_node.item}")
+        self.head= new_node
+        print(f"self.head.item: {self.head.item}")
+        print(f"self.head.ref: {self.head.ref}")
+        if self.head.ref is not None:
+               print(f"self.head.ref.item: {self.head.ref.item}")
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head= new_node
+            return
+        n = self.head
+        while n.ref is not None:
+            n= n.ref
+        n.ref = new_node;
+
+#In the above script, we create a function insert_at_end(), which inserts the element at the end of the linked list. The value of the item that we want to insert is passed as an argument to the function. The function consists of two parts. First we check if the linked list is empty or not, if the linked list is empty, all we have to do is set the value of the start_node variable to new_node object.
+##
+#On the other hand, if the list already contains some nodes. We initialize a variable n with the start node. We then iterate through all the nodes in the list using a while loop 
+
+    def traverse_list(self):
+        if self.head is None:
+           print("List has no element")
+           return
+        else:
+            n = self.head
+            while n is not None:
+                print(n.item , " ")
+                n = n.ref
+
+
+
+if __name__=="__main__":
+    ll = LinkedList()
+    print(ll)
+    ll.insert_at_end(11)
+    ll.insert_at_end(22)
+    ll.insert_at_end(33)
+    ll.insert_at_end(44)
+    ll.insert_at_end(55)
+    ll.traverse_list()
+
+
+### must refer the link https://stackabuse.com/linked-lists-in-detail-with-python-examples-single-linked-lists/ , good example and also dont forget to read reverse linked list logic at the end
